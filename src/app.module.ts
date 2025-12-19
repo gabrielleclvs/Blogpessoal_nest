6637217441d3c6046c17801c1b8a34153dddbede
Gabrielle De Castro <gabrielleclvs@users.noreply.github.com>
2025-12-18 21:40:35 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Postagem } from './postagem/entities/postagem.entity';
-import { PostagemModule } from './postagem/postagem.module';
+import { TemaModule } from './tema/tema.module';
+import { Tema } from './tema/entities/tema.entity';
 import { AuthModule } from './auth/auth.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { Tema } from './tema/entities/tema.entity';
-import { TemaModule } from './tema/tema.module';
+import { ProdService } from './data/services/prod.service';
+import { PostagemModule } from './postagem/postagem.module';
 
-// Decorator - Etiqueta de Metadados
 @Module({
-  imports: [  // Configurando o TypeORM
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     PostagemModule,
     TemaModule,
--- src/auth/bcrypt/bcrypt.ts
+++ src/auth/bcrypt/bcrypt.ts
@@ -4,13 +4,13 @@ import * as bcrypt from 'bcrypt';
 @Injectable()   // Indica que é uma Classe de Serviço e pode ser inserida/injetada em outras classes diretamente
 export class Bcrypt {
 
-    // Função responsavel por criptografar a senha o usuário
+    // Função responsavel por criptografar a senha do usuário
     async criptografarSenha(senha: string): Promise<string> {
-        let saltos: number = 10;    // Determina o qual forte a criptografia será
+        let saltos: number = 10;    // Determina o quão forte a criptografia será
         return await bcrypt.hash(senha, saltos)
     }
 
-    // Função responsavel por comparar a senha criptografa e salva no banco com a sem enviada no login
+    // Função responsavel por comparar a senha criptografada e salva no banco com a sem enviada no login
     async compararSenhas(senhaBanco: string, senhaDigitada: string): Promise<boolean> {
         return bcrypt.compareSync(senhaDigitada, senhaBanco);
     }
